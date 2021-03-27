0168ac31c47ccffb4421cc7ebb583bb364a51ff6
ny <ss.porttown1624@gmail.com>
2021-03-27 15:55:43 +0900

    Initial Commit

---
+++ Jiei-shoubou-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jiei-shoubou-test
+//
+//  Created by ny on 2021/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
