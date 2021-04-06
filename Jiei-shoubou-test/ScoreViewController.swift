ed5ca619c468c6e6d5a401485a786d22c10629ad
nakada0426 <ss.porttown1624@gmail.com>
2021-04-06 16:33:32 +0900

    “if分岐レビュー後修正”

--- Jiei-shoubou-test/ScoreViewController.swift
+++ Jiei-shoubou-test/ScoreViewController.swift
@@ -6,30 +6,23 @@
 //
 
 import UIKit
-
-
 class ScoreViewController: UIViewController {
-  
-    
-    @IBOutlet var commentlavel: UILabel!
+    @IBOutlet var Resultlavel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
     var correct = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if ( correct > 10 ) {
-            commentlavel.text = "すばらしい!"
-        } else {
-            commentlavel.text = "残念！"
-        }
-
-        scoreLabel.text = "\(correct)問正解!"
         
-        // Do any additional setup after loading the view.
+        if  correct > 20  {
+            Resultlavel.text = "すばらしい！合格圏内です！"
+        } else if correct > 14 {
+            Resultlavel.text = "惜しい!あと少し！"
+        } else {
+            Resultlavel.text = "もう少し勉強が必要です"
+        }
+        scoreLabel.text = "\(correct)問正解!"
     }
-
-    
     @IBAction func shareButtonAction(_ sender: Any) {
         let activityItems = ["\(correct)問正解しました！", "#自衛消防技術試験アプリ"]
         let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
@@ -40,13 +33,4 @@ class ScoreViewController: UIViewController {
         self.presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: true)
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     */
 }
