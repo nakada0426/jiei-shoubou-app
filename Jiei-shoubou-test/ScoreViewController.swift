a3a29cc8b8000f565aca02692cbc261778ffecd6
nakada0426 <ss.porttown1624@gmail.com>
2021-04-09 13:11:20 +0900

    “インタースティシャル実装”

--- Jiei-shoubou-test/ScoreViewController.swift
+++ Jiei-shoubou-test/ScoreViewController.swift
@@ -4,25 +4,54 @@
 //
 //  Created by ny on 2021/03/27.
 //
-
 import UIKit
-class ScoreViewController: UIViewController {
-    @IBOutlet var Resultlavel: UILabel!
+import GoogleMobileAds
+class ScoreViewController: UIViewController , GADFullScreenContentDelegate {
+    @IBOutlet var resultlavel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
     var correct = 0
+    private var interstitial: GADInterstitialAd?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if  correct > 20  {
-            Resultlavel.text = "すばらしい！合格圏内です！"
+        if correct > 20  {
+            resultlavel.text = "すばらしい！合格圏内です！"
         } else if correct > 14 {
-            Resultlavel.text = "惜しい!あと少し！"
+            resultlavel.text = "惜しい!あと少し！"
         } else {
-            Resultlavel.text = "もう少し勉強が必要です"
+            resultlavel.text = "もう少し勉強が必要です"
         }
         scoreLabel.text = "\(correct)問正解!"
     }
+    
+    func createInterAd(){
+        let request = GADRequest()
+        GADInterstitialAd.load(withAdUnitID:"ca-app-pub-3940256099942544/4411468910",
+                               request: request,
+                               completionHandler: { [self] ad, error in
+                                if let error = error {
+                                    print("Failed 失敗です　to load interstitial ad with error: \(error.localizedDescription)")
+                                    return
+                                }
+                                interstitial = ad
+                                interstitial?.fullScreenContentDelegate = self
+                               }
+        )
+    }
+
+    func ad(_ ad: GADFullScreenPresentingAd, didFailToPresentFullScreenContentWithError error: Error) {
+        print("広告表示の失敗　Ad did fail to present full screen content.")
+    }
+    func adDidPresentFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        print("広告表示の成功　Ad did present full screen content.")
+    }
+    func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        print("広告表示を消す　Ad did dismiss full screen content.")
+        createInterAd()
+    }
+    
+
     @IBAction func shareButtonAction(_ sender: Any) {
         let activityItems = ["\(correct)問正解しました！", "#自衛消防技術試験アプリ"]
         let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
