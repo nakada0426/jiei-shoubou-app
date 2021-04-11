b58b68253e1479ee22e8fa2bb19dff529c89e94b
nakada0426 <ss.porttown1624@gmail.com>
2021-04-11 17:14:30 +0900

    “インタースティシャル広告実装２”

--- Jiei-shoubou-test/ScoreViewController.swift
+++ Jiei-shoubou-test/ScoreViewController.swift
@@ -10,11 +10,22 @@ class ScoreViewController: UIViewController , GADFullScreenContentDelegate {
     @IBOutlet var resultlavel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
     var correct = 0
-    private var interstitial: GADInterstitialAd?
+    private var interstitial: GADInterstitialAd!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let request = GADRequest()
+        GADInterstitialAd.load(withAdUnitID:"ca-app-pub-3940256099942544/4411468910",
+                               request: request,
+                               completionHandler: { [self] ad, error in
+                                if let error = error {
+                                    print("Failed 失敗です　to load interstitial ad with error: \(error.localizedDescription)")
+                                    return
+                                }
+                                interstitial = ad
+                                interstitial?.fullScreenContentDelegate = self
+                               }
+        )
         if correct > 20  {
             resultlavel.text = "すばらしい！合格圏内です！"
         } else if correct > 14 {
@@ -48,10 +59,9 @@ class ScoreViewController: UIViewController , GADFullScreenContentDelegate {
     }
     func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
         print("広告表示を消す　Ad did dismiss full screen content.")
-        createInterAd()
+        self.presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: true)
     }
     
-
     @IBAction func shareButtonAction(_ sender: Any) {
         let activityItems = ["\(correct)問正解しました！", "#自衛消防技術試験アプリ"]
         let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
@@ -60,6 +70,12 @@ class ScoreViewController: UIViewController , GADFullScreenContentDelegate {
     
     @IBAction func toTopButtonAction(_ sender: Any) {
         self.presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: true)
+        if interstitial != nil {
+          interstitial.present(fromRootViewController: self)
+        } else {
+          print("Ad wasn't ready")
+        }
+
     }
-    
+
 }
